70fa59521889ca44f16872668ad661c7710c01c1
lcx06 <lucian160403@gmail.com>
2022-05-28 16:46:29 +0200

    Añadidos tipos a los datos

--- src/api/data.js
+++ src/api/data.js
@@ -1,404 +1,451 @@
 const data = {
   transactions: [
     {
-      "date": Date.parse("Mar 5, 2021"),
-      "amount": 39
-    },
-    {
-      "date": Date.parse("Jul 2, 2021"),
-      "amount": 27
-    },
-    {
-      "date": Date.parse("Mar 12, 2021"),
-      "amount": 6
-    },
-    {
-      "date": Date.parse("Apr 26, 2021"),
+      "date": Date.parse("Jan 9, 2021"),
       "amount": 73
     },
     {
-      "date": Date.parse("Aug 21, 2021"),
-      "amount": 78
-    },
-    {
-      "date": Date.parse("Dec 4, 2021"),
-      "amount": 105
-    },
-    {
-      "date": Date.parse("Mar 2, 2022"),
-      "amount": 89
-    },
-    {
-      "date": Date.parse("Jan 16, 2021"),
-      "amount": 118
-    },
-    {
-      "date": Date.parse("Aug 7, 2021"),
-      "amount": 39
-    },
-    {
-      "date": Date.parse("Jan 17, 2021"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("Mar 29, 2021"),
-      "amount": 43
-    },
-    {
-      "date": Date.parse("Jul 14, 2021"),
-      "amount": 93
-    },
-    {
-      "date": Date.parse("Apr 11, 2022"),
-      "amount": 33
-    },
-    {
-      "date": Date.parse("Feb 27, 2022"),
+      "date": Date.parse("Mar 22, 2022"),
       "amount": 15
     },
     {
-      "date": Date.parse("Apr 8, 2021"),
-      "amount": 85
+      "date": Date.parse("Jun 16, 2021"),
+      "amount": 63
     },
     {
-      "date": Date.parse("Oct 17, 2021"),
-      "amount": 117
+      "date": Date.parse("Jul 13, 2021"),
+      "amount": 87,
+      "type": "Screens"
     },
     {
-      "date": Date.parse("Jan 10, 2021"),
-      "amount": 64
+      "date": Date.parse("Sep 19, 2021"),
+      "amount": 66,
+      "type": "Screens"
     },
     {
-      "date": Date.parse("Jan 5, 2021"),
+      "date": Date.parse("Oct 28, 2021"),
+      "amount": 47,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("May 4, 2022"),
+      "amount": 46,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Apr 23, 2022"),
+      "amount": 61,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Apr 14, 2021"),
+      "amount": 44
+    },
+    {
+      "date": Date.parse("Jul 23, 2021"),
+      "amount": 103
+    },
+    {
+      "date": Date.parse("Feb 15, 2021"),
+      "amount": 46
+    },
+    {
+      "date": Date.parse("Aug 21, 2021"),
+      "amount": 41
+    },
+    {
+      "date": Date.parse("Jul 30, 2021"),
+      "amount": 64,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Jan 22, 2022"),
       "amount": 119
     },
     {
-      "date": Date.parse("Jun 7, 2022"),
-      "amount": 110
+      "date": Date.parse("Sep 30, 2021"),
+      "amount": 99,
+      "type": "Phones"
     },
     {
-      "date": Date.parse("Mar 13, 2022"),
-      "amount": 42
+      "date": Date.parse("Feb 26, 2021"),
+      "amount": 41,
+      "type": "Screens"
     },
     {
-      "date": Date.parse("Jan 18, 2022"),
-      "amount": 17
+      "date": Date.parse("Feb 17, 2022"),
+      "amount": 35,
+      "type": "Screens"
     },
     {
-      "date": Date.parse("Apr 9, 2022"),
-      "amount": 29
+      "date": Date.parse("May 30, 2021"),
+      "amount": 107,
+      "type": "PCs"
     },
     {
-      "date": Date.parse("Oct 14, 2021"),
-      "amount": 3
+      "date": Date.parse("May 12, 2022"),
+      "amount": 106
     },
     {
-      "date": Date.parse("Apr 15, 2021"),
-      "amount": 69
+      "date": Date.parse("Feb 27, 2021"),
+      "amount": 48
     },
     {
-      "date": Date.parse("Feb 19, 2021"),
-      "amount": 93
+      "date": Date.parse("Apr 24, 2021"),
+      "amount": 42,
+      "type": "Phones"
     },
     {
-      "date": Date.parse("Jun 29, 2021"),
-      "amount": 115
+      "date": Date.parse("May 8, 2022"),
+      "amount": 60,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Sep 6, 2021"),
+      "amount": 89
+    },
+    {
+      "date": Date.parse("Jan 1, 2021"),
+      "amount": 101,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Jan 26, 2021"),
+      "amount": 13,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Jun 24, 2022"),
+      "amount": 73,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Feb 27, 2021"),
+      "amount": 11
+    },
+    {
+      "date": Date.parse("Apr 24, 2021"),
+      "amount": 13
+    },
+    {
+      "date": Date.parse("Apr 27, 2021"),
+      "amount": 88
+    },
+    {
+      "date": Date.parse("Jun 3, 2022"),
+      "amount": 8,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Apr 1, 2021"),
+      "amount": 55
+    },
+    {
+      "date": Date.parse("Jun 9, 2022"),
+      "amount": 86,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Jun 5, 2022"),
+      "amount": 95
+    },
+    {
+      "date": Date.parse("Aug 20, 2021"),
+      "amount": 32,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Mar 28, 2021"),
+      "amount": 67,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Apr 11, 2022"),
+      "amount": 83,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Jul 16, 2021"),
+      "amount": 37,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Apr 24, 2021"),
+      "amount": 19
+    },
+    {
+      "date": Date.parse("Feb 2, 2022"),
+      "amount": 47
+    },
+    {
+      "date": Date.parse("Jun 20, 2022"),
+      "amount": 57
+    },
+    {
+      "date": Date.parse("Sep 12, 2021"),
+      "amount": 38
+    },
+    {
+      "date": Date.parse("Jun 19, 2022"),
+      "amount": 87,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Oct 30, 2021"),
+      "amount": 110,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("May 12, 2022"),
+      "amount": 46,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Sep 8, 2021"),
+      "amount": 24
+    },
+    {
+      "date": Date.parse("Oct 10, 2021"),
+      "amount": 30,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Feb 28, 2021"),
+      "amount": 61,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Feb 7, 2022"),
+      "amount": 112
+    },
+    {
+      "date": Date.parse("Aug 13, 2021"),
+      "amount": 76,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Aug 7, 2021"),
+      "amount": 26
+    },
+    {
+      "date": Date.parse("Jun 9, 2021"),
+      "amount": 2
+    },
+    {
+      "date": Date.parse("May 26, 2022"),
+      "amount": 82
+    },
+    {
+      "date": Date.parse("May 22, 2021"),
+      "amount": 24
+    },
+    {
+      "date": Date.parse("Aug 22, 2021"),
+      "amount": 61,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Feb 17, 2021"),
+      "amount": 70
+    },
+    {
+      "date": Date.parse("May 7, 2022"),
+      "amount": 50
+    },
+    {
+      "date": Date.parse("Apr 29, 2021"),
+      "amount": 84,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Feb 10, 2022"),
+      "amount": 100
+    },
+    {
+      "date": Date.parse("Apr 7, 2021"),
+      "amount": 10,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Nov 11, 2021"),
+      "amount": 75
+    },
+    {
+      "date": Date.parse("Jan 23, 2021"),
+      "amount": 1
+    },
+    {
+      "date": Date.parse("May 28, 2022"),
+      "amount": 48
+    },
+    {
+      "date": Date.parse("Jun 18, 2022"),
+      "amount": 46
+    },
+    {
+      "date": Date.parse("May 28, 2021"),
+      "amount": 2,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Mar 17, 2022"),
+      "amount": 119,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Mar 28, 2022"),
+      "amount": 108
     },
     {
       "date": Date.parse("Mar 3, 2021"),
+      "amount": 84,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Nov 15, 2021"),
+      "amount": 51,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Jan 18, 2022"),
+      "amount": 30,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Jul 22, 2021"),
+      "amount": 57,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Nov 8, 2021"),
+      "amount": 42
+    },
+    {
+      "date": Date.parse("Dec 1, 2021"),
+      "amount": 100
+    },
+    {
+      "date": Date.parse("Feb 19, 2021"),
+      "amount": 29,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Jan 5, 2022"),
+      "amount": 26,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Jun 27, 2021"),
+      "amount": 95
+    },
+    {
+      "date": Date.parse("Aug 9, 2021"),
+      "amount": 28,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Apr 28, 2022"),
+      "amount": 113,
+      "type": "PCs"
+    },
+    {
+      "date": Date.parse("Apr 9, 2022"),
       "amount": 96
     },
     {
-      "date": Date.parse("Mar 6, 2021"),
-      "amount": 61
+      "date": Date.parse("Apr 16, 2022"),
+      "amount": 39
     },
     {
-      "date": Date.parse("Sep 7, 2021"),
+      "date": Date.parse("Sep 22, 2021"),
+      "amount": 64
+    },
+    {
+      "date": Date.parse("Jun 22, 2022"),
+      "amount": 83
+    },
+    {
+      "date": Date.parse("Oct 18, 2021"),
+      "amount": 89,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("Dec 28, 2021"),
+      "amount": 106
+    },
+    {
+      "date": Date.parse("May 11, 2021"),
+      "amount": 101,
+      "type": "Screens"
+    },
+    {
+      "date": Date.parse("Mar 23, 2021"),
+      "amount": 102
+    },
+    {
+      "date": Date.parse("Oct 7, 2021"),
+      "amount": 8,
+      "type": "Phones"
+    },
+    {
+      "date": Date.parse("May 15, 2022"),
       "amount": 95
     },
     {
       "date": Date.parse("Jun 22, 2022"),
-      "amount": 89
+      "amount": 33
     },
     {
-      "date": Date.parse("Sep 14, 2021"),
-      "amount": 92
+      "date": Date.parse("May 2, 2021"),
+      "amount": 8
     },
     {
-      "date": Date.parse("Sep 27, 2021"),
-      "amount": 67
+      "date": Date.parse("Apr 29, 2021"),
+      "amount": 59
     },
     {
-      "date": Date.parse("Mar 6, 2021"),
-      "amount": 19
+      "date": Date.parse("Jan 25, 2022"),
+      "amount": 116,
+      "type": "PCs"
     },
     {
-      "date": Date.parse("Feb 15, 2022"),
-      "amount": 73
-    },
-    {
-      "date": Date.parse("Sep 18, 2021"),
-      "amount": 53
-    },
-    {
-      "date": Date.parse("Mar 1, 2021"),
-      "amount": 68
-    },
-    {
-      "date": Date.parse("Mar 12, 2022"),
-      "amount": 93
-    },
-    {
-      "date": Date.parse("May 11, 2022"),
-      "amount": 25
-    },
-    {
-      "date": Date.parse("May 13, 2022"),
-      "amount": 23
-    },
-    {
-      "date": Date.parse("Apr 1, 2022"),
-      "amount": 38
-    },
-    {
-      "date": Date.parse("May 31, 2022"),
-      "amount": 35
-    },
-    {
-      "date": Date.parse("Dec 19, 2021"),
-      "amount": 14
-    },
-    {
-      "date": Date.parse("Jan 18, 2021"),
-      "amount": 28
-    },
-    {
-      "date": Date.parse("Sep 6, 2021"),
-      "amount": 27
-    },
-    {
-      "date": Date.parse("May 20, 2022"),
-      "amount": 3
-    },
-    {
-      "date": Date.parse("Dec 9, 2021"),
-      "amount": 74
-    },
-    {
-      "date": Date.parse("Dec 26, 2021"),
-      "amount": 106
-    },
-    {
-      "date": Date.parse("Dec 27, 2021"),
-      "amount": 29
-    },
-    {
-      "date": Date.parse("Jun 30, 2021"),
-      "amount": 94
-    },
-    {
-      "date": Date.parse("Dec 18, 2021"),
-      "amount": 79
-    },
-    {
-      "date": Date.parse("Feb 6, 2022"),
-      "amount": 19
-    },
-    {
-      "date": Date.parse("Jun 26, 2022"),
-      "amount": 86
-    },
-    {
-      "date": Date.parse("Jun 16, 2022"),
-      "amount": 11
-    },
-    {
-      "date": Date.parse("May 9, 2022"),
-      "amount": 80
-    },
-    {
-      "date": Date.parse("Apr 28, 2022"),
-      "amount": 16
-    },
-    {
-      "date": Date.parse("Mar 5, 2022"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("Oct 13, 2021"),
-      "amount": 44
-    },
-    {
-      "date": Date.parse("Dec 7, 2021"),
-      "amount": 6
-    },
-    {
-      "date": Date.parse("Apr 19, 2022"),
-      "amount": 7
-    },
-    {
-      "date": Date.parse("Jun 10, 2022"),
-      "amount": 23
-    },
-    {
-      "date": Date.parse("Jul 23, 2021"),
-      "amount": 114
-    },
-    {
-      "date": Date.parse("Jun 25, 2021"),
-      "amount": 55
-    },
-    {
-      "date": Date.parse("Feb 24, 2022"),
-      "amount": 35
-    },
-    {
-      "date": Date.parse("Jun 22, 2021"),
-      "amount": 103
-    },
-    {
-      "date": Date.parse("Jul 30, 2021"),
-      "amount": 80
-    },
-    {
-      "date": Date.parse("Aug 28, 2021"),
-      "amount": 85
-    },
-    {
-      "date": Date.parse("Jun 18, 2022"),
-      "amount": 116
-    },
-    {
-      "date": Date.parse("Jan 23, 2021"),
-      "amount": 64
-    },
-    {
-      "date": Date.parse("Dec 3, 2021"),
-      "amount": 86
-    },
-    {
-      "date": Date.parse("Jan 30, 2022"),
-      "amount": 97
-    },
-    {
-      "date": Date.parse("Mar 9, 2021"),
-      "amount": 60
-    },
-    {
-      "date": Date.parse("May 27, 2022"),
-      "amount": 79
-    },
-    {
-      "date": Date.parse("Jan 29, 2021"),
-      "amount": 99
-    },
-    {
-      "date": Date.parse("Feb 11, 2022"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("Mar 14, 2021"),
-      "amount": 42
-    },
-    {
-      "date": Date.parse("Jan 3, 2021"),
-      "amount": 5
-    },
-    {
-      "date": Date.parse("Sep 21, 2021"),
-      "amount": 12
-    },
-    {
-      "date": Date.parse("Apr 17, 2022"),
-      "amount": 44
-    },
-    {
-      "date": Date.parse("Feb 22, 2021"),
-      "amount": 47
-    },
-    {
-      "date": Date.parse("Sep 24, 2021"),
-      "amount": 75
-    },
-    {
-      "date": Date.parse("Jan 28, 2022"),
-      "amount": 23
-    },
-    {
-      "date": Date.parse("Oct 9, 2021"),
-      "amount": 49
+      "date": Date.parse("Dec 5, 2021"),
+      "amount": 98,
+      "type": "Phones"
     },
     {
       "date": Date.parse("May 31, 2021"),
-      "amount": 55
+      "amount": 47,
+      "type": "PCs"
     },
     {
-      "date": Date.parse("Nov 20, 2021"),
-      "amount": 10
+      "date": Date.parse("Feb 13, 2022"),
+      "amount": 85
     },
     {
-      "date": Date.parse("Jul 27, 2021"),
-      "amount": 93
+      "date": Date.parse("Jun 8, 2021"),
+      "amount": 86
     },
     {
-      "date": Date.parse("Jul 20, 2021"),
-      "amount": 68
+      "date": Date.parse("Jan 2, 2021"),
+      "amount": 32
     },
     {
-      "date": Date.parse("Jun 24, 2022"),
-      "amount": 5
+      "date": Date.parse("Jun 11, 2021"),
+      "amount": 33
     },
     {
-      "date": Date.parse("Jan 22, 2021"),
-      "amount": 65
+      "date": Date.parse("Mar 7, 2022"),
+      "amount": 87,
+      "type": "Phones"
     },
     {
-      "date": Date.parse("Dec 10, 2021"),
-      "amount": 98
+      "date": Date.parse("Jun 17, 2021"),
+      "amount": 63
     },
     {
-      "date": Date.parse("Oct 20, 2021"),
-      "amount": 76
-    },
-    {
-      "date": Date.parse("Apr 25, 2021"),
-      "amount": 74
-    },
-    {
-      "date": Date.parse("Feb 22, 2022"),
-      "amount": 56
-    },
-    {
-      "date": Date.parse("Oct 14, 2021"),
-      "amount": 27
-    },
-    {
-      "date": Date.parse("Jul 18, 2021"),
-      "amount": 27
-    },
-    {
-      "date": Date.parse("Jun 13, 2021"),
-      "amount": 95
-    },
-    {
-      "date": Date.parse("Dec 22, 2021"),
-      "amount": 51
-    },
-    {
-      "date": Date.parse("Jun 6, 2022"),
-      "amount": 69
-    },
-    {
-      "date": Date.parse("Feb 2, 2022"),
-      "amount": 54
-    },
-    {
-      "date": Date.parse("May 4, 2021"),
-      "amount": 54
-    },
-    {
-      "date": Date.parse("Nov 30, 2021"),
-      "amount": 57
+      "date": Date.parse("Apr 24, 2021"),
+      "amount": 113
     }
   ]
 }
