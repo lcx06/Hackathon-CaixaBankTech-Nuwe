27a36101a0bd3bb825733a83546ab65ad8b72622
lcx06 <lucian160403@gmail.com>
2022-05-28 11:21:40 +0200

    Ajustes basicos iniciales

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>NeoBanks</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
