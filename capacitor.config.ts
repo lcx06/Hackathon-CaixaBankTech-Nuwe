e379a73ca71d1183d5415717a824f3c7f5fff82c
lcx06 <lucian160403@gmail.com>
2022-05-28 10:53:33 +0200

    Añadido soporte para android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.lcx06',
   appName: 'caixabank_hackathon',
   webDir: 'build',
   bundledWebRuntime: false
