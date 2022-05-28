25252887fee4699a0e018a0b7275ff8437237fb7
lcx06 <lucian160403@gmail.com>
2022-05-28 19:21:07 +0200

    Tamaño asignado al logo

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -15,7 +15,7 @@ const Dashboard: React.FC<ContainerProps> = (props) => {
         <IonPage>
             <IonHeader className={"ion-no-border"}>
                 <IonToolbar className={"logo-toolbar"}>
-                    <img className={"logo-neo"} src={"/assets/icon/logo_first.png"} alt={"Logo"} />
+                    <img className={"logo-neo"} src={"/assets/icon/logo_first.png"} alt={"Logo"} width={69} height={24} />
                     <span className={"logo-banks " + (Capacitor.isNativePlatform() ? "logo-banks-native" : "")}>BANKS</span>
                     {!Capacitor.isNativePlatform() && <div className={"toolbar-right"}>
                         <object type="image/svg+xml" data={"/assets/svg/cog.svg"} className={"toolbar-right-icon"} />
