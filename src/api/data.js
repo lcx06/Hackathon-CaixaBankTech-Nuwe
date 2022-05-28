c48171654f2e04d3cd3da5be082ee6a3e14fafc9
lcx06 <lucian160403@gmail.com>
2022-05-28 15:11:10 +0200

    Datos actualizados

--- src/api/data.js
+++ src/api/data.js
@@ -1,404 +1,404 @@
 const data = {
   transactions: [
     {
-      "date": Date.parse("May 19, 2022"),
-      "amount": 63
-    },
-    {
-      "date": Date.parse("Mar 11, 2022"),
-      "amount": 14
-    },
-    {
-      "date": Date.parse("Jan 24, 2022"),
-      "amount": 28
-    },
-    {
-      "date": Date.parse("Apr 15, 2022"),
-      "amount": 12
-    },
-    {
-      "date": Date.parse("Apr 8, 2022"),
-      "amount": 8
-    },
-    {
-      "date": Date.parse("Mar 11, 2022"),
-      "amount": 43
-    },
-    {
-      "date": Date.parse("May 3, 2022"),
-      "amount": 54
-    },
-    {
-      "date": Date.parse("Mar 13, 2022"),
-      "amount": 26
-    },
-    {
-      "date": Date.parse("Mar 12, 2022"),
-      "amount": 91
-    },
-    {
-      "date": Date.parse("Jan 31, 2022"),
-      "amount": 113
-    },
-    {
-      "date": Date.parse("Mar 20, 2022"),
-      "amount": 56
-    },
-    {
-      "date": Date.parse("Feb 4, 2022"),
-      "amount": 77
-    },
-    {
-      "date": Date.parse("May 7, 2022"),
-      "amount": 106
-    },
-    {
-      "date": Date.parse("Apr 13, 2022"),
-      "amount": 115
-    },
-    {
-      "date": Date.parse("Mar 11, 2022"),
-      "amount": 85
-    },
-    {
-      "date": Date.parse("Apr 14, 2022"),
-      "amount": 40
-    },
-    {
-      "date": Date.parse("Jan 10, 2022"),
-      "amount": 40
-    },
-    {
-      "date": Date.parse("May 22, 2022"),
-      "amount": 105
-    },
-    {
-      "date": Date.parse("Jan 16, 2022"),
-      "amount": 38
-    },
-    {
-      "date": Date.parse("Feb 28, 2022"),
-      "amount": 22
-    },
-    {
-      "date": Date.parse("Jan 9, 2022"),
-      "amount": 49
-    },
-    {
-      "date": Date.parse("Jan 9, 2022"),
-      "amount": 4
-    },
-    {
-      "date": Date.parse("Jan 4, 2022"),
-      "amount": 90
-    },
-    {
-      "date": Date.parse("May 1, 2022"),
-      "amount": 112
-    },
-    {
-      "date": Date.parse("Apr 27, 2022"),
-      "amount": 20
-    },
-    {
-      "date": Date.parse("Mar 25, 2022"),
-      "amount": 53
-    },
-    {
-      "date": Date.parse("May 15, 2022"),
-      "amount": 112
-    },
-    {
-      "date": Date.parse("Mar 6, 2022"),
-      "amount": 65
-    },
-    {
-      "date": Date.parse("May 9, 2022"),
-      "amount": 66
-    },
-    {
-      "date": Date.parse("Apr 8, 2022"),
-      "amount": 88
-    },
-    {
-      "date": Date.parse("May 10, 2022"),
-      "amount": 99
-    },
-    {
-      "date": Date.parse("Apr 11, 2022"),
-      "amount": 81
-    },
-    {
-      "date": Date.parse("May 14, 2022"),
-      "amount": 26
-    },
-    {
-      "date": Date.parse("Feb 9, 2022"),
-      "amount": 55
-    },
-    {
-      "date": Date.parse("Apr 30, 2022"),
-      "amount": 25
-    },
-    {
-      "date": Date.parse("Jan 9, 2022"),
-      "amount": 99
-    },
-    {
-      "date": Date.parse("Mar 21, 2022"),
-      "amount": 63
-    },
-    {
-      "date": Date.parse("May 6, 2022"),
-      "amount": 61
-    },
-    {
-      "date": Date.parse("Jan 24, 2022"),
-      "amount": 51
-    },
-    {
-      "date": Date.parse("May 23, 2022"),
-      "amount": 70
-    },
-    {
-      "date": Date.parse("Apr 4, 2022"),
-      "amount": 101
-    },
-    {
-      "date": Date.parse("Apr 26, 2022"),
-      "amount": 94
-    },
-    {
-      "date": Date.parse("Apr 22, 2022"),
-      "amount": 99
-    },
-    {
-      "date": Date.parse("May 16, 2022"),
-      "amount": 76
-    },
-    {
-      "date": Date.parse("Jan 17, 2022"),
-      "amount": 92
-    },
-    {
-      "date": Date.parse("May 2, 2022"),
-      "amount": 105
-    },
-    {
-      "date": Date.parse("Jan 20, 2022"),
-      "amount": 109
-    },
-    {
-      "date": Date.parse("May 27, 2022"),
-      "amount": 111
-    },
-    {
-      "date": Date.parse("May 2, 2022"),
-      "amount": 13
-    },
-    {
-      "date": Date.parse("Apr 12, 2022"),
-      "amount": 47
-    },
-    {
-      "date": Date.parse("Mar 17, 2022"),
-      "amount": 81
-    },
-    {
-      "date": Date.parse("Mar 5, 2022"),
-      "amount": 87
-    },
-    {
-      "date": Date.parse("Apr 25, 2022"),
-      "amount": 6
-    },
-    {
-      "date": Date.parse("Feb 3, 2022"),
-      "amount": 68
-    },
-    {
-      "date": Date.parse("May 11, 2022"),
-      "amount": 45
-    },
-    {
-      "date": Date.parse("May 16, 2022"),
-      "amount": 25
-    },
-    {
-      "date": Date.parse("Feb 5, 2022"),
-      "amount": 64
-    },
-    {
-      "date": Date.parse("Jan 20, 2022"),
-      "amount": 12
-    },
-    {
-      "date": Date.parse("May 4, 2022"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("Apr 18, 2022"),
-      "amount": 58
-    },
-    {
-      "date": Date.parse("Apr 7, 2022"),
-      "amount": 11
-    },
-    {
-      "date": Date.parse("Jan 23, 2022"),
-      "amount": 117
-    },
-    {
-      "date": Date.parse("Jan 26, 2022"),
-      "amount": 5
-    },
-    {
-      "date": Date.parse("Apr 2, 2022"),
-      "amount": 70
-    },
-    {
-      "date": Date.parse("Mar 30, 2022"),
-      "amount": 34
-    },
-    {
-      "date": Date.parse("May 6, 2022"),
-      "amount": 31
-    },
-    {
-      "date": Date.parse("May 22, 2022"),
-      "amount": 93
-    },
-    {
-      "date": Date.parse("Apr 17, 2022"),
-      "amount": 59
-    },
-    {
-      "date": Date.parse("May 20, 2022"),
-      "amount": 44
-    },
-    {
-      "date": Date.parse("Jan 27, 2022"),
-      "amount": 98
-    },
-    {
-      "date": Date.parse("Jan 26, 2022"),
-      "amount": 70
-    },
-    {
-      "date": Date.parse("Feb 5, 2022"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("May 9, 2022"),
-      "amount": 31
-    },
-    {
-      "date": Date.parse("Feb 27, 2022"),
-      "amount": 71
-    },
-    {
-      "date": Date.parse("Apr 2, 2022"),
-      "amount": 114
-    },
-    {
-      "date": Date.parse("Jan 22, 2022"),
-      "amount": 46
-    },
-    {
-      "date": Date.parse("Jan 3, 2022"),
-      "amount": 113
-    },
-    {
-      "date": Date.parse("Apr 5, 2022"),
-      "amount": 34
-    },
-    {
-      "date": Date.parse("May 19, 2022"),
-      "amount": 48
-    },
-    {
-      "date": Date.parse("Feb 2, 2022"),
-      "amount": 43
-    },
-    {
-      "date": Date.parse("Jan 7, 2022"),
-      "amount": 13
-    },
-    {
-      "date": Date.parse("Apr 28, 2022"),
-      "amount": 48
-    },
-    {
-      "date": Date.parse("May 23, 2022"),
-      "amount": 24
-    },
-    {
-      "date": Date.parse("Jan 7, 2022"),
-      "amount": 14
-    },
-    {
-      "date": Date.parse("Apr 15, 2022"),
-      "amount": 101
-    },
-    {
-      "date": Date.parse("Mar 7, 2022"),
-      "amount": 111
-    },
-    {
-      "date": Date.parse("Feb 18, 2022"),
-      "amount": 95
-    },
-    {
-      "date": Date.parse("Apr 25, 2022"),
-      "amount": 45
-    },
-    {
-      "date": Date.parse("May 23, 2022"),
-      "amount": 101
-    },
-    {
-      "date": Date.parse("Mar 24, 2022"),
-      "amount": 26
-    },
-    {
-      "date": Date.parse("Mar 19, 2022"),
-      "amount": 13
-    },
-    {
-      "date": Date.parse("Feb 22, 2022"),
-      "amount": 115
-    },
-    {
-      "date": Date.parse("Apr 16, 2022"),
-      "amount": 80
-    },
-    {
-      "date": Date.parse("Mar 11, 2022"),
-      "amount": 105
-    },
-    {
-      "date": Date.parse("Jan 31, 2022"),
+      "date": Date.parse("Apr 12, 2021"),
       "amount": 78
     },
     {
-      "date": Date.parse("Apr 28, 2022"),
-      "amount": 29
+      "date": Date.parse("Jul 6, 2021"),
+      "amount": 61
     },
     {
-      "date": Date.parse("May 16, 2022"),
+      "date": Date.parse("Jun 18, 2021"),
+      "amount": 11
+    },
+    {
+      "date": Date.parse("Jun 17, 2021"),
+      "amount": 51
+    },
+    {
+      "date": Date.parse("Oct 26, 2021"),
       "amount": 15
     },
     {
-      "date": Date.parse("Feb 26, 2022"),
-      "amount": 37
+      "date": Date.parse("Oct 15, 2021"),
+      "amount": 13
     },
     {
-      "date": Date.parse("Feb 15, 2022"),
+      "date": Date.parse("Jun 24, 2021"),
+      "amount": 40
+    },
+    {
+      "date": Date.parse("Oct 27, 2021"),
+      "amount": 39
+    },
+    {
+      "date": Date.parse("Aug 28, 2021"),
+      "amount": 18
+    },
+    {
+      "date": Date.parse("Mar 4, 2021"),
+      "amount": 16
+    },
+    {
+      "date": Date.parse("Jan 17, 2021"),
+      "amount": 40
+    },
+    {
+      "date": Date.parse("Aug 27, 2021"),
       "amount": 96
     },
     {
-      "date": Date.parse("Mar 6, 2022"),
-      "amount": 4
+      "date": Date.parse("Nov 21, 2021"),
+      "amount": 23
+    },
+    {
+      "date": Date.parse("Feb 13, 2021"),
+      "amount": 91
+    },
+    {
+      "date": Date.parse("Feb 28, 2021"),
+      "amount": 23
+    },
+    {
+      "date": Date.parse("Jun 11, 2021"),
+      "amount": 78
+    },
+    {
+      "date": Date.parse("Jul 27, 2021"),
+      "amount": 42
+    },
+    {
+      "date": Date.parse("Nov 2, 2021"),
+      "amount": 73
+    },
+    {
+      "date": Date.parse("Nov 8, 2021"),
+      "amount": 66
+    },
+    {
+      "date": Date.parse("Dec 13, 2021"),
+      "amount": 106
+    },
+    {
+      "date": Date.parse("Jan 10, 2021"),
+      "amount": 100
+    },
+    {
+      "date": Date.parse("Dec 4, 2021"),
+      "amount": 119
+    },
+    {
+      "date": Date.parse("May 25, 2021"),
+      "amount": 119
+    },
+    {
+      "date": Date.parse("Jan 28, 2021"),
+      "amount": 67
+    },
+    {
+      "date": Date.parse("Jan 27, 2021"),
+      "amount": 11
+    },
+    {
+      "date": Date.parse("May 3, 2021"),
+      "amount": 72
+    },
+    {
+      "date": Date.parse("May 3, 2021"),
+      "amount": 14
+    },
+    {
+      "date": Date.parse("Nov 29, 2021"),
+      "amount": 40
+    },
+    {
+      "date": Date.parse("Jul 25, 2021"),
+      "amount": 26
+    },
+    {
+      "date": Date.parse("Oct 30, 2021"),
+      "amount": 12
+    },
+    {
+      "date": Date.parse("Dec 3, 2021"),
+      "amount": 5
+    },
+    {
+      "date": Date.parse("Jul 31, 2021"),
+      "amount": 84
+    },
+    {
+      "date": Date.parse("Mar 31, 2021"),
+      "amount": 35
+    },
+    {
+      "date": Date.parse("Oct 28, 2021"),
+      "amount": 59
+    },
+    {
+      "date": Date.parse("Jul 18, 2021"),
+      "amount": 116
+    },
+    {
+      "date": Date.parse("Jul 8, 2021"),
+      "amount": 46
+    },
+    {
+      "date": Date.parse("Sep 10, 2021"),
+      "amount": 98
+    },
+    {
+      "date": Date.parse("Feb 18, 2021"),
+      "amount": 12
+    },
+    {
+      "date": Date.parse("Mar 7, 2021"),
+      "amount": 82
+    },
+    {
+      "date": Date.parse("Nov 13, 2021"),
+      "amount": 45
+    },
+    {
+      "date": Date.parse("Feb 10, 2021"),
+      "amount": 34
+    },
+    {
+      "date": Date.parse("May 2, 2021"),
+      "amount": 40
+    },
+    {
+      "date": Date.parse("Apr 1, 2021"),
+      "amount": 68
+    },
+    {
+      "date": Date.parse("Feb 28, 2021"),
+      "amount": 79
+    },
+    {
+      "date": Date.parse("Nov 17, 2021"),
+      "amount": 111
+    },
+    {
+      "date": Date.parse("Oct 7, 2021"),
+      "amount": 95
+    },
+    {
+      "date": Date.parse("Nov 11, 2021"),
+      "amount": 111
+    },
+    {
+      "date": Date.parse("Oct 26, 2021"),
+      "amount": 40
+    },
+    {
+      "date": Date.parse("Aug 20, 2021"),
+      "amount": 2
+    },
+    {
+      "date": Date.parse("Jul 7, 2021"),
+      "amount": 83
+    },
+    {
+      "date": Date.parse("Nov 19, 2021"),
+      "amount": 51
+    },
+    {
+      "date": Date.parse("Apr 20, 2021"),
+      "amount": 81
+    },
+    {
+      "date": Date.parse("Jan 28, 2021"),
+      "amount": 85
+    },
+    {
+      "date": Date.parse("Jan 15, 2021"),
+      "amount": 12
+    },
+    {
+      "date": Date.parse("Jan 24, 2021"),
+      "amount": 33
+    },
+    {
+      "date": Date.parse("Jul 19, 2021"),
+      "amount": 104
+    },
+    {
+      "date": Date.parse("Mar 12, 2021"),
+      "amount": 31
+    },
+    {
+      "date": Date.parse("Aug 2, 2021"),
+      "amount": 19
+    },
+    {
+      "date": Date.parse("Mar 16, 2021"),
+      "amount": 42
+    },
+    {
+      "date": Date.parse("Mar 8, 2021"),
+      "amount": 81
+    },
+    {
+      "date": Date.parse("Jun 1, 2021"),
+      "amount": 43
+    },
+    {
+      "date": Date.parse("Jun 22, 2021"),
+      "amount": 82
+    },
+    {
+      "date": Date.parse("Jun 28, 2021"),
+      "amount": 73
+    },
+    {
+      "date": Date.parse("Feb 10, 2021"),
+      "amount": 74
+    },
+    {
+      "date": Date.parse("May 8, 2021"),
+      "amount": 89
+    },
+    {
+      "date": Date.parse("Jul 31, 2021"),
+      "amount": 116
+    },
+    {
+      "date": Date.parse("Aug 8, 2021"),
+      "amount": 60
+    },
+    {
+      "date": Date.parse("Mar 14, 2021"),
+      "amount": 50
+    },
+    {
+      "date": Date.parse("Sep 21, 2021"),
+      "amount": 51
+    },
+    {
+      "date": Date.parse("Nov 10, 2021"),
+      "amount": 31
+    },
+    {
+      "date": Date.parse("Jul 24, 2021"),
+      "amount": 118
+    },
+    {
+      "date": Date.parse("Sep 7, 2021"),
+      "amount": 25
+    },
+    {
+      "date": Date.parse("Sep 12, 2021"),
+      "amount": 55
+    },
+    {
+      "date": Date.parse("Sep 29, 2021"),
+      "amount": 82
+    },
+    {
+      "date": Date.parse("Nov 18, 2021"),
+      "amount": 66
+    },
+    {
+      "date": Date.parse("Mar 4, 2021"),
+      "amount": 6
+    },
+    {
+      "date": Date.parse("Jul 25, 2021"),
+      "amount": 52
+    },
+    {
+      "date": Date.parse("May 5, 2021"),
+      "amount": 89
+    },
+    {
+      "date": Date.parse("Jul 2, 2021"),
+      "amount": 51
+    },
+    {
+      "date": Date.parse("Jul 16, 2021"),
+      "amount": 85
+    },
+    {
+      "date": Date.parse("Dec 22, 2021"),
+      "amount": 119
+    },
+    {
+      "date": Date.parse("Aug 14, 2021"),
+      "amount": 66
+    },
+    {
+      "date": Date.parse("Nov 7, 2021"),
+      "amount": 75
+    },
+    {
+      "date": Date.parse("Dec 9, 2021"),
+      "amount": 37
+    },
+    {
+      "date": Date.parse("Feb 28, 2021"),
+      "amount": 28
+    },
+    {
+      "date": Date.parse("Dec 7, 2021"),
+      "amount": 30
+    },
+    {
+      "date": Date.parse("Sep 3, 2021"),
+      "amount": 70
+    },
+    {
+      "date": Date.parse("Sep 3, 2021"),
+      "amount": 28
+    },
+    {
+      "date": Date.parse("May 5, 2021"),
+      "amount": 31
+    },
+    {
+      "date": Date.parse("Sep 17, 2021"),
+      "amount": 29
+    },
+    {
+      "date": Date.parse("Mar 22, 2021"),
+      "amount": 42
+    },
+    {
+      "date": Date.parse("Jul 28, 2021"),
+      "amount": 65
+    },
+    {
+      "date": Date.parse("Mar 26, 2021"),
+      "amount": 27
+    },
+    {
+      "date": Date.parse("Jun 22, 2021"),
+      "amount": 20
+    },
+    {
+      "date": Date.parse("Dec 21, 2021"),
+      "amount": 78
+    },
+    {
+      "date": Date.parse("Jul 22, 2021"),
+      "amount": 29
+    },
+    {
+      "date": Date.parse("Sep 27, 2021"),
+      "amount": 108
+    },
+    {
+      "date": Date.parse("Nov 1, 2021"),
+      "amount": 33
+    },
+    {
+      "date": Date.parse("Feb 25, 2021"),
+      "amount": 117
+    },
+    {
+      "date": Date.parse("Sep 6, 2021"),
+      "amount": 71
     }
   ]
 }
