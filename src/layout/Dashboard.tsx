9527e61618f8de309418c427e0d609ce030801c9
lcx06 <lucian160403@gmail.com>
2022-05-28 13:37:10 +0200

    Añadido footer

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -1,7 +1,9 @@
 import './Dashboard.css';
-import {IonContent, IonHeader, IonPage, IonToolbar} from "@ionic/react";
+import {IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonToolbar} from "@ionic/react";
 import {FunctionComponent} from "react";
 import {Capacitor} from "@capacitor/core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChartSimple, faCompass, faUser} from "@fortawesome/free-solid-svg-icons";
 
 interface ContainerProps {
     component: FunctionComponent;
@@ -15,11 +17,29 @@ const Dashboard: React.FC<ContainerProps> = (props) => {
                 <IonToolbar className={Capacitor.isNativePlatform() ? "logo-native" : ""}>
                     <img className={"logo-neo"} src={"/assets/icon/logo_first.png"} alt={"Logo"} />
                     <span className={"logo-banks " + (Capacitor.isNativePlatform() ? "logo-banks-native" : "")}>BANKS</span>
+                    {!Capacitor.isNativePlatform() && <div className={"toolbar-right"}>
+                        <img src={"/assets/svg/cog.svg"} alt={"Cog"} width={22} height={22} />
+                    </div>}
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
                 <props.component {...props} />
             </IonContent>
+            {Capacitor.isNativePlatform() && <div className={"footer"}>
+                <IonGrid>
+                    <IonRow className={"footer-items"}>
+                        <IonCol className={"selected"}>
+                            <FontAwesomeIcon icon={faChartSimple} size={"lg"}/>
+                        </IonCol>
+                        <IonCol>
+                            <FontAwesomeIcon icon={faCompass} size={"lg"}/>
+                        </IonCol>
+                        <IonCol>
+                            <FontAwesomeIcon icon={faUser} size={"lg"}/>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </div>}
         </IonPage>
     );
 };
