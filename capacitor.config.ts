f1b764a367ec07e096100913b3299e7967fd6975
lcx06 <lucian160403@gmail.com>
2022-05-28 20:06:22 +0200

    Cambios en el nombre e icono de la APP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'dev.lcx06',
-  appName: 'caixabank_hackathon',
+  appName: 'NeoBanks',
   webDir: 'build',
   bundledWebRuntime: false
 };
